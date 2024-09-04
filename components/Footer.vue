d6a083bc91e6ef1c315190604ec28a01fc638496
Pedro Resende <pedroresende@mail.resende.biz>
2024-09-04 12:09:07 +0100

    feat: add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="p-4 bg-gray-100 text-gray-800">
+    <div class="container flex justify-between ml-4 lg:mx-auto">
+      <Author :author="{ name: 'Pedro Resende' }" />
+    </div>
+    </footer>
+</template>
