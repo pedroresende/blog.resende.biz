0f7129eeeab1cbb6eb45e6c79fc7f5c28b1e32e1
Pedro Resende <pedroresende@mail.resende.biz>
2024-12-23 11:57:49 +0000

    feat: added @nuxthq/studio package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/content", "@nuxt/image", "nuxt-delay-hydration"],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/image",
+    "nuxt-delay-hydration",
+    "@nuxthq/studio"
+  ],
   delayHydration: {
     debug: process.env.node === "development"
   },
