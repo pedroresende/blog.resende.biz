04328f9be56ce0766b4b64b8b25baa1c680ac865
Pedro Resende <pedroresende@mail.resende.biz>
2025-08-04 11:06:44 +0100

    feat: Nuxtlink should only trigger on prefetchOn interaction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,57 @@
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  experimental: {
+    defaults: {
+      nuxtLink: {
+        prefetchOn: "interaction",
+      },
+    },
+  },
   app: {
     head: {
       meta: [
         {
           name: "google-site-verification",
-          content: "z-WmJHNUAoibdq5ifYvXqoUtMySomvIOL05IEFn6pv0"
-        }
-      ]
+          content: "z-WmJHNUAoibdq5ifYvXqoUtMySomvIOL05IEFn6pv0",
+        },
+      ],
     },
     pageTransition: {
       name: "page",
-      mode: "out-in"
-    }
+      mode: "out-in",
+    },
   },
   routeRules: {
     "**": {
       headers: {
-        "frame-ancestors": "https://help.disqus.com/"
-      }
-    }
+        "frame-ancestors": "https://help.disqus.com/",
+      },
+    },
   },
   image: {
-    domains: ["blog.resende.biz"]
+    domains: ["blog.resende.biz"],
   },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   css: ["~/assets/css/main.css"],
   modules: ["@nuxt/content", "@nuxt/image", "nuxt-delay-hydration"],
   delayHydration: {
-    debug: process.env.node === "development"
+    debug: process.env.node === "development",
   },
   nitro: {
     prerender: {
       autoSubfolderIndex: false,
-      routes: ["/sitemap.xml"]
-    }
+      routes: ["/sitemap.xml"],
+    },
   },
   content: {
     preview: {
-      api: "https://api.nuxt.studio"
-    }
-  }
-})
+      api: "https://api.nuxt.studio",
+    },
+  },
+});
