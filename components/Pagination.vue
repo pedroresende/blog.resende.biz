70fde00adefd2fedfc8225964a224c2e3ab3860a
PMMR <951377+pedroresende@users.noreply.github.com>
2026-07-19 11:21:31 +0100

    fix: hover button carregar mais

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,12 +1,12 @@
 <script setup>
-const emit = defineEmits(["loadMorePosts"])
+const emit = defineEmits(["loadMorePosts"]);
 </script>
 
 <template>
   <div class="w-full text-center">
     <button
       @click="emit('loadMorePosts')"
-      class="p-2 rounded-md dark:hover:text-green-700 hover:bg-inherit border mt-4 bg-green-700 text-white"
+      class="p-2 rounded-md dark:hover:text-green-700 hover:bg-inherit hover:border-green-700 border mt-4 bg-green-700 text-white hover:text-green-700 cursor-pointer"
     >
       Carregar Mais Posts
     </button>
